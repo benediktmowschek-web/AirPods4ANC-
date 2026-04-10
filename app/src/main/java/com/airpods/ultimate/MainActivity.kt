17ce3a108d8986e05e5e04f415f47b3b1e3aa15a
benediktmowschek-web <benediktmowschek@googlemail.com>
2026-04-10 16:26:38 +0200

    Initialize MainActivity with basic structure

---
+++ app/src/main/java/com/airpods/ultimate/MainActivity.kt
@@ -0,0 +1,12 @@
+
+package com.airpods.ultimate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
