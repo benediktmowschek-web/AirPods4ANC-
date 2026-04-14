19b43c0b3d85cf0f8837a282c6a9f18ca44c530f
benediktmowschek-web <benediktmowschek@googlemail.com>
2026-04-14 08:22:43 +0200

    Update BluetoothManager.kt

--- app/src/main/java/com/airpods/ultimate/BluetoothManager.kt
+++ app/src/main/java/com/airpods/ultimate/BluetoothManager.kt
@@ -1,15 +1,11 @@
-package com.airpods4anc
+package com.airpods.ultimate
 
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
 
 object BluetoothManager {
 
-    fun getAirPods(): BluetoothDevice? {
-        val adapter = BluetoothAdapter.getDefaultAdapter() ?: return null
-
-        return adapter.bondedDevices.firstOrNull {
-            it.name.contains("AirPods", true)
-        }
-    }
+    fun getAirPods() =
+        BluetoothAdapter.getDefaultAdapter()
+            ?.bondedDevices
+            ?.firstOrNull { it.name.contains("AirPods", true) }
 }
